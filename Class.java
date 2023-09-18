589297b96a8c0dac37845a7cb9b5aed8f4908745
rlaehddn0301 <128763302+rlaehddn0301@users.noreply.github.com>
2023-09-18 23:35:00 +0900

    Add files via upload

---
+++ Class.java
@@ -0,0 +1,10 @@
+package week0301;
+
+public class Class {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ Variable.java
@@ -0,0 +1,13 @@
+package week0301;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		int height;
+		double size = 0.25;
+		double total = height + size;
+		char c = 'a';
+		String name = "김동우";
+	}
+
+}
